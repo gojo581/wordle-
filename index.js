7f893672166dfe6607f42c52ab0c710d928c12ca
gojo581 <gadi17levin@gmail.com>
2025-05-30 13:38:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,85 +1,62 @@
-// function c(div1){
-
-// if(div1 == "tiger")  alert("corect") || window.location.replace("file:///C:/Users/gadi1/OneDrive/%D7%A9%D7%95%D7%9C%D7%97%D7%9F%20%D7%94%D7%A2%D7%91%D7%95%D7%93%D7%94/%D7%91%D7%99%D7%AA%20%D7%A1%D7%A4%D7%A8/lets%20go.html")
-// else alert("not corect") || alert("yo the oger is here! he will kill you!") || window.location.replace("file:///C:/Users/gadi1/OneDrive/%D7%A9%D7%95%D7%9C%D7%97%D7%9F%20%D7%94%D7%A2%D7%91%D7%95%D7%93%D7%94/%D7%91%D7%99%D7%AA%20%D7%A1%D7%A4%D7%A8/ogi.html")
-
-
-// }
-
-// function tryluck(){
-// let mass = document.getElementById("tryluck")
-// let mass1=mass.value
-// c(mass1)
- 
-
-// }
-
-// function ogi1(){
-// alert("hajdhkasjhdsfisb😂😂😈😈") || window.location.replace("file:///C:/Users/gadi1/OneDrive/%D7%A9%D7%95%D7%9C%D7%97%D7%9F%20%D7%94%D7%A2%D7%91%D7%95%D7%93%D7%94/%D7%91%D7%99%D7%AA%20%D7%A1%D7%A4%D7%A8/lol.html")
-// }
-
-
-// function tryagain(){
-// window.location.replace("file:///C:/Users/gadi1/OneDrive/%D7%A9%D7%95%D7%9C%D7%97%D7%9F%20%D7%94%D7%A2%D7%91%D7%95%D7%93%D7%94/%D7%91%D7%99%D7%AA%20%D7%A1%D7%A4%D7%A8/m.html")
-
-// }
-
-
-
-let word = 'soul'
-let mispar= 0
+let mispar = 0
 let skibidi = 6
-function guess(){
 
+let words = [
+  "word", "test", "code", "game", "play", "quiz", "like",
+  "blue", "list", "node",  "must", "data", "most",
+  "miss", "lord", "rest", "five", "four"
+]
 
-    let inputElement = document.getElementById('tryluck')
-    let div1 = document.getElementById('div')
-    
+let toGuess = words[randomInteger(0, words.length - 1)]
+console.log("🔍 Secret word:", toGuess) // for testing
 
+let inputElement = document.getElementById('wordId')
+let container = document.getElementById('container')
 
-    
-    let firstLetter=inputElement.value.charAt(0)
-    let secondLetter=inputElement.value.charAt(1)
-    let thirdLetter=inputElement.value.charAt(2)
-    let forthLetter=inputElement.value.charAt(3)
-    mispar=mispar+1
- div1.innerHTML+=`
+function check() {
+  let guess = inputElement.value.toLowerCase()
 
+  if (guess.length !== 4) {
+    alert("המילה חייבת להיות באורך של 4 אותיות!")
+    return
+  }
 
+  let rowHTML = `<div class="guess-row">`
 
-<div class="letter"style="background-color:${check(firstLetter,0)};" >${firstLetter}</div>
-<div class="letter"style="background-color:${check(secondLetter,1)};" >${ secondLetter}</div>
-<div class="letter"style="background-color:${check(thirdLetter,2)};" >${thirdLetter}</div>
-<div class="letter"style="background-color:${check(forthLetter,3)};" >${forthLetter}</div>
- `
-if(word==inputElement.value){alert('you win');
-    mispar=0
+  for (let i = 0; i < 4; i++) {
+    let letter = guess.charAt(i)
+    rowHTML += `<div class="letter" style="background-color:${color(letter, i)};">${letter}</div>`
+  }
+
+  rowHTML += `</div>`
+  container.innerHTML += rowHTML
+  mispar++
+
+  if (guess === toGuess) {
+    alert("🎉 ניחשת נכון! ניצחת!")
+    mispar = 0
+    window.location.reload()
+  }
+
+  if (mispar === skibidi) {
+    alert(`😢 הפסדת! המילה הייתה: ${toGuess}`)
+    window.location.reload()
+  }
+
+  inputElement.value = ""
+  inputElement.focus()
 }
 
- if(mispar==skibidi){alert("you lose you are a big loser lol")||window.location.replace('https://gojo581.github.io/wordle-/')}
-
-}
-
-function check(letter, index){
-if(word.charAt(index)==letter){
+function color(letter, index) {
+  if (toGuess.charAt(index) === letter) {
     return 'green'
-}
-if(word.includes(letter)){
+  }
+  if (toGuess.includes(letter)) {
     return 'yellow'
-}
-return 'red'
-}
-
-
-
-
-
-
-
-function yoo(){
-alert("you are a normal and you are good!👍") || window.location.replace('https://gojo581.github.io/wordle-/')
-
+  }
+  return 'red'
 }
 
-
- 
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
